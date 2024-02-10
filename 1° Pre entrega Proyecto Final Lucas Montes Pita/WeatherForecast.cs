e2cbd9664cdff0e08745d40bd04666c15a7d1132
Lucas Montes <153567608+lucasmontes04@users.noreply.github.com>
2024-02-10 18:43:28 -0300

    Add files via upload

---
+++ 1° Pre entrega Proyecto Final Lucas Montes Pita/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace _1__Pre_entrega_Proyecto_Final_Lucas_Montes_Pita
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
